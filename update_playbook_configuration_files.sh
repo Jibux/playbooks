e84e85db48f6d0446fd505389ecf696882e04984
Jibux <jibux@jibux.info>
2019-05-22 22:42:02 +0200

    Add raspberry role
    Add hdparm.conf
    [raspberry] disable wifi
    Add update_playbook_configuration_files.sh

---
+++ update_playbook_configuration_files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+COPY_CMD="rsync -a"
+HOST="jibux-server"
+
+copy() {
+	$COPY_CMD $HOST:$2 roles/$1/files/$3
+}
+
+copy raspberry /etc/modprobe.d/raspi-blacklist.conf raspi-blacklist.conf
+
+copy misc /root/.bashrc root_bashrc
+copy misc /var/spool/cron/crontabs/root cron_root
+
